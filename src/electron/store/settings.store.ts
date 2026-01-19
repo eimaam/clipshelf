afd9a111086eb55b0767a6a15bb8882aa7e050c0
Imam Dahir <imamddahir@gmail.com>
2026-01-19 12:46:15 +0100

    feat: implement settings store with defs

---
+++ src/electron/store/settings.store.ts
@@ -0,0 +1,62 @@
+import Store from "electron-store"
+import { IClipboardSettings } from "../types/clipboard.types"
+
+
+const settingStoreSchema = {
+    maxClips: {
+        type: "number",
+        default: 100 // mvp def
+    },
+    launchOnStartup: {
+        type: "boolean",
+        default: true
+    },
+    showCopyNotification: {
+        type: "boolean",
+        default: true
+    },
+    retentionDays: {
+        type: "number",
+        default: 7
+    },
+    globalShortcut: {
+        type: "string",
+        default: "Command+Shift+C"
+    }
+}
+
+const settingStore = new Store<IClipboardSettings>({ schema: settingStoreSchema } as any)
+
+
+export class SettingsStore {
+    static getSettings() :IClipboardSettings{
+        return settingStore.store
+    }
+
+    static getSetting<K extends keyof IClipboardSettings>(key: K): IClipboardSettings[K] {
+        return settingStore.get(key)
+    }
+
+    static setSettings(settings: IClipboardSettings) {
+        return settingStore.set(settings)
+    }
+
+    static setSetting<K extends keyof IClipboardSettings>(key: K, value: IClipboardSettings[K]) {
+        settingStore.set(key, value)
+    }
+
+    static resetSettings() {
+        settingStore.reset()
+    }
+
+    static resetSetting<K extends keyof IClipboardSettings>(key: K) {
+        settingStore.reset(key)
+    }
+
+
+    static onChange(callback: (settings: IClipboardSettings) => void) {
+        settingStore.onDidAnyChange((newValue) => {
+            callback(newValue as IClipboardSettings)
+        })
+    }
+}
